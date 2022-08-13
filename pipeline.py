d8f4eb75b918c7059158e230ca7d13c8ddb43b6f
Daniel Valdivia <hola@danielvaldivia.com>
2022-08-12 19:14:22 -0700

    Kubeflow Pipeline

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,7 @@
 import kfp
 import kfp.components as comp
 import requests
+from kfp.compiler import Compiler
 from kfp.components import InputPath, OutputPath
 from kfp.dsl import pipeline
 
@@ -129,21 +130,12 @@ def preprocess_data(file: str,
         file_list = []
         for i in range(total_training):
             counter += 1
-            prev_t = timeit.default_timer() - starttime
             sentence_embedding = embed([records[i]["text"]])
-            emb_t = timeit.default_timer() - starttime - prev_t
             label_encoded = encode_label(records[i]["label"])
-            lab_t = timeit.default_timer() - starttime - prev_t - emb_t
             record = serialize_example(label_encoded, sentence_embedding)
-            rec_t = timeit.default_timer() - starttime - prev_t - emb_t - lab_t
             buffer.append(record)
-            print(
-                f"\rDone serializing example ( {emb_t} - {lab_t} - {rec_t} ). Buffer {len(buffer)} - {timeit.default_timer() - starttime}",
-                end="")
 
             if len(buffer) >= tf_record_file_size:
-                print("")
-                print(f"Records in buffer {len(buffer)} - {timeit.default_timer() - starttime}")
                 # save this buffer of examples as a file to MinIO
                 counter = 0
                 file_counter += 1
@@ -152,8 +144,7 @@ def preprocess_data(file: str,
                     with tf.io.TFRecordWriter(f.name, options="GZIP") as writer:
                         for example in buffer:
                             writer.write(example.SerializeToString())
-                print(
-                    f"done write example file, size {os.stat(file_name).st_size} - {timeit.default_timer() - starttime}")
+
                 try:
                     minioClient.fput_object(datasets_bucket, f"{preprocessed_data_folder}/{file_name}", file_name)
                 except S3Error as err:
@@ -185,7 +176,6 @@ def preprocess_data(file: str,
     with open(output_text_path, 'w') as writer:
         writer.write("done!")
 
-
     print("Done!")
 
 
@@ -214,16 +204,10 @@ def train(train_path: InputPath(),
                         secure=False)
 
     # List all training tfrecord files
-    objects = minioClient.list_objects(datasets_bucket, prefix="train")
+    objects = minioClient.list_objects(datasets_bucket, prefix=f"{preprocessed_data_folder}/train")
     training_files_list = []
     for obj in objects:
         training_files_list.append(obj.object_name)
-
-    # List all testing tfrecord files
-    objects = minioClient.list_objects(datasets_bucket, prefix="test")
-    testing_files_list = []
-    for obj in objects:
-        testing_files_list.append(obj.object_name)
 
     # Configure TF to use MinIO
     os.environ["AWS_ACCESS_KEY_ID"] = minio_access_key
@@ -234,7 +218,6 @@ def train(train_path: InputPath(),
     os.environ["S3_VERIFY_SSL"] = "0"
 
     all_training_filenames = [f"s3://datasets/{f}" for f in training_files_list]
-    testing_filenames = [f"s3://datasets/{f}" for f in testing_files_list]
 
     total_train_data_files = math.floor(len(all_training_filenames) * 0.9)
     if total_train_data_files == len(all_training_filenames):
@@ -251,9 +234,6 @@ def train(train_path: InputPath(),
 
     validation = tf.data.TFRecordDataset(validation_files, num_parallel_reads=AUTO, compression_type="GZIP")
     validation = validation.with_options(ignore_order)
-
-    testing_dataset = tf.data.TFRecordDataset(testing_filenames, num_parallel_reads=AUTO, compression_type="GZIP")
-    testing_dataset = testing_dataset.with_options(ignore_order)
 
     def decode_fn(record_bytes):
         schema = {
@@ -307,10 +287,6 @@ def train(train_path: InputPath(),
     mapped_validation = mapped_validation.repeat(5)
     mapped_validation = mapped_validation.batch(128)
 
-    testing_mapped_ds = testing_dataset.map(decode_fn)
-    testing_mapped_ds = testing_mapped_ds.repeat(5)
-    testing_mapped_ds = testing_mapped_ds.batch(128)
-
     checkpoint_path = f"s3://{datasets_bucket}/checkpoints/cp.ckpt"
     cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                      save_weights_only=True,
@@ -329,32 +305,99 @@ def train(train_path: InputPath(),
 
     model_destination = f"s3://{datasets_bucket}/imdb_sentiment_analysis/1"
     model.save(model_destination)
-
+    with open(output_text_path, 'w') as writer:
+        writer.write("done training!")
     print("Done!")
 
 
 @pipeline(
-    name='Download from MinIO',
+    name='Evaluate Model',
 )
-def download_from_minio(url: InputPath('Url'), data: OutputPath('Tarball')):
+def evaluate(train_path: InputPath(),
+             output_text_path: OutputPath(str)):
+    import tensorflow as tf
     from minio import Minio
-    print(url)
-    client = Minio(
-        "ns-1.kind-1.dc.min.dev",
-        access_key="kubeflow",
-        secret_key="kubeflow123",
-    )
+    from tensorflow import keras
+    import os
 
-    client.fget_object("datasets", "aclImdb_v1.tar.gz", data)
+    # Config Paramters
+    minio_address = "minio.ns-1.svc.cluster.local"
+    minio_access_key = "kubeflow"
+    minio_secret_key = "kubeflow123"
+    datasets_bucket = "datasets"
+
+    preprocessed_data_folder = "preprocessed-data"
+    tf_record_file_size = 500
+
+    # Configure TF to use MinIO
+    os.environ["AWS_ACCESS_KEY_ID"] = minio_access_key
+    os.environ["AWS_SECRET_ACCESS_KEY"] = minio_secret_key
+    os.environ["AWS_REGION"] = "us-east-1"
+    os.environ["S3_ENDPOINT"] = minio_address
+    os.environ["S3_USE_HTTPS"] = "0"
+    os.environ["S3_VERIFY_SSL"] = "0"
+
+    minioClient = Minio(minio_address,
+                        access_key=minio_access_key,
+                        secret_key=minio_secret_key,
+                        secure=False)
+
+    AUTO = tf.data.experimental.AUTOTUNE
+    ignore_order = tf.data.Options()
+    ignore_order.experimental_deterministic = False
+
+    # List all testing tfrecord files
+    objects = minioClient.list_objects(datasets_bucket, prefix=f"{preprocessed_data_folder}/test")
+    testing_files_list = []
+    for obj in objects:
+        testing_files_list.append(obj.object_name)
+
+    testing_filenames = [f"s3://datasets/{f}" for f in testing_files_list]
+
+    testing_dataset = tf.data.TFRecordDataset(testing_filenames, num_parallel_reads=AUTO, compression_type="GZIP")
+    testing_dataset = testing_dataset.with_options(ignore_order)
+
+    def decode_fn(record_bytes):
+        schema = {
+            "label": tf.io.FixedLenFeature([2], dtype=tf.int64),
+            "sentence": tf.io.FixedLenFeature([512], dtype=tf.float32),
+        }
+
+        tf_example = tf.io.parse_single_example(record_bytes, schema)
+        new_shape = tf.reshape(tf_example["sentence"], [1, 512])
+        label = tf.reshape(tf_example["label"], [1, 2])
+        return new_shape, label
+
+    testing_mapped_ds = testing_dataset.map(decode_fn)
+    testing_mapped_ds = testing_mapped_ds.repeat(5)
+    testing_mapped_ds = testing_mapped_ds.batch(128)
+
+    # load model
+    model_destination = f"s3://{datasets_bucket}/imdb_sentiment_analysis/1"
+
+    model = keras.models.load_model(model_destination)
+
+    testing = model.evaluate(testing_mapped_ds)
+
+    print(testing)
+
+    with open(output_text_path, 'w') as writer:
+        writer.write("done eval!")
 
 
-download_from_minio_op = kfp.components.create_component_from_func(
-    func=download_from_minio,
-    output_component_file='component-download.yaml',  # This is optional. It saves the component spec for future use.
-    base_image='python:3.7',
-    packages_to_install=['minio==7.1.10', 'tensorflow==2.2.3'], )
+@pipeline(
+    name='Deploy Model',
+)
+def serve(train_path: InputPath(),
+          output_text_path: OutputPath(str)):
+    print("Deploy")
+    from kubernetes import client, config
 
-create_step_preprocess_data = kfp.components.create_component_from_func(
+    with open(output_text_path, 'w') as writer:
+        writer.write("done training!")
+
+
+preprocess_data_op = kfp.components.create_component_from_func(
     func=preprocess_data,
     output_component_file='component-pre-process.yaml',  # This is optional. It saves the component spec for future use.
     base_image='python:3.7',
@@ -363,7 +406,7 @@ create_step_preprocess_data = kfp.components.create_component_from_func(
         'tensorflow==2.2.3',
         'protobuf==3.20.0',
     ])
-create_step_train = kfp.components.create_component_from_func(
+train_op = kfp.components.create_component_from_func(
     func=train,
     output_component_file='component-train.yaml',  # This is optional. It saves the component spec for future use.
     base_image='python:3.7',
@@ -372,15 +415,36 @@ create_step_train = kfp.components.create_component_from_func(
         'tensorflow==2.2.3',
         'protobuf==3.20.0',
     ])
+evaluate_op = kfp.components.create_component_from_func(
+    func=evaluate,
+    output_component_file='component-evaluate.yaml',  # This is optional. It saves the component spec for future use.
+    base_image='python:3.7',
+    packages_to_install=[
+        'minio==7.1.10',
+        'tensorflow==2.2.3',
+        'protobuf==3.20.0',
+        'matplotlib',
+        'numpy',
+    ])
+serve_op = kfp.components.create_component_from_func(
+    func=serve,
+    output_component_file='component-serve.yaml',  # This is optional. It saves the component spec for future use.
+    base_image='python:3.7',
+    packages_to_install=[
+        'minio==7.1.10',
+        'tensorflow==2.2.3',
+        'protobuf==3.20.0',
+        'kubernetes',
+    ])
 
 
 # Define a pipeline and create a task from a component:
 def my_pipeline(url):
-    download_from_minio_task = download_from_minio_op(url=url)
-    print(download_from_minio_task.outputs)
-    merge_csv_task_train = create_step_preprocess_data(file="train")
-    # merge_csv_task_test = create_step_preprocess_data(file="test")
-    train = create_step_train(train_path=merge_csv_task_train)
+    preprocess_task = preprocess_data_op(file="train")
+    print(train_op.__code__.co_varnames)
+    train_task = train_op(train=preprocess_task.output)
+    evaluate_task = evaluate_op(train=train_task.output)
+    serve_task = serve_op(train=evaluate_task.output)
     # The outputs of the merge_csv_task can be referenced using the
     # merge_csv_task.outputs dictionary: merge_csv_task.outputs['output_csv']
 
@@ -390,7 +454,7 @@ def run():
     #     pipeline_func=my_pipeline,
     #     package_path='pipeline-v3.yaml')
 
-    # HOST = "https://kubeflow.kind-1.dc.min.dev"
+    # HOST = "https://kubeflow.tes.dc.min.dev"
     HOST = "http://kubeflow.lulu"
     # HOST = "http://localhost:3000"
     # AUTH_HOST = "https://kubeflow.lulu"
@@ -424,13 +488,17 @@ def run():
     print(client.list_experiments())
     x = client.create_run_from_pipeline_func(
         my_pipeline,
-        run_name="minio_download",
+        run_name="sentiment_analysis_model",
         namespace=NAMESPACE,
         arguments={
             'url': 'https://storage.googleapis.com/ml-pipeline-playground/iris-csv-files.tar.gz'
         })
     print(x)
 
+def compile():
+    Compiler().compile(my_pipeline, 'sentiment-analysis-object-storage.yaml')
+
 
 if __name__ == "__main__":
     run()
+    # compile()
